f828718bcf752bf54186988062b9f64421ff310e
Nick Kamzelas <nickbrick@hotmail.com>
2021-02-14 05:02:25 +0200

    Add image saving location setting

--- vizzy/Properties/Settings.Designer.cs
+++ vizzy/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace vizzy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,15 @@ namespace vizzy.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\Nick\\Desktop")]
+        public string ImageSaveLocation {
+            get {
+                return ((string)(this["ImageSaveLocation"]));
             }
         }
     }
